1fee6e7882fde575976cd9899f18db9a0d74898a
OB1cobi <jacobiglenn@yahoo.com>
2023-04-06 11:35:03 -0700

    made lists

--- G1/Assets/Scripts/CharChange/CharSelect.cs
+++ G1/Assets/Scripts/CharChange/CharSelect.cs
@@ -6,23 +6,45 @@ public class CharSelect : MonoBehaviour
 {
     public GameObject[] characters; // list of characters to be selected
     public int selectedChar = 0;
+    public GameObject[] cammy;
+    public int selectedCam;
 
+    private void Start()
+    {
+        PlayerPrefs.SetInt("selectedCam", 0);
+    }
     public void NextChar()
     {
-        characters[selectedChar].SetActive(false); //sets current selection as false
+        //player next activation
+        characters[selectedChar].SetActive(false); // sets current selection as false
         selectedChar = (selectedChar + 1) % characters.Length; // moves up one in the list of characters
         characters[selectedChar].SetActive(true); // activates the new selection
+
+        //cam next activation
+        cammy[selectedCam].SetActive(false); // set current cam as false
+        selectedCam = (selectedCam + 1) % cammy.Length; // moves up one in the list of cammy's (cameras)
+        cammy[selectedCam].SetActive(true); // activates new camera
     }
 
     public void PreviousCharacter()
     {
-        characters[selectedChar].SetActive(false);//sets current selection as false
+        //player prev activation
+        characters[selectedChar].SetActive(false); // sets current character as false
         selectedChar--;
-        if (selectedChar <0)
+        if (selectedChar < 0)
         {
-            selectedChar += characters.Length;// moves back one in the list of characters
+            selectedChar += characters.Length; // moves back one in the list of characters
         }
-        characters[selectedChar].SetActive(true);// activates the new selection
+        characters[selectedChar].SetActive(true); // activates the new selection
+
+        //cam prev activation
+        cammy[selectedCam].SetActive(false); // setws current cam as false
+        selectedCam--;
+        if (selectedCam < 0)
+        {
+
+        }
+
     }
 
     public void Selected__()
@@ -32,6 +54,8 @@ public class CharSelect : MonoBehaviour
         {
             PlayerPrefs.SetInt("selectedChar", 1);
         }
+
+
     }
 
 
